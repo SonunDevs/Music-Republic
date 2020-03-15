9d6ca2e78067e3e5a326234ae8449c3ce9236cc6
Nurjigit <45354207+Nurjigit-Melis-uulu@users.noreply.github.com>
2020-03-15 22:56:50 +0600

    changed header style

--- frontend/src/components/ui/Header/Header.js
+++ frontend/src/components/ui/Header/Header.js
@@ -4,6 +4,10 @@ import classes from "./Header.module.css";
 function Header(props) {
   return (
     <div className={classes.Header}>
+      <img
+        src="https://zort.ru/media/uploads/images/artists/daft-punk_314.jpeg"
+        alt="header"
+      />
       <div className={classes.headerPlay}>
         <h3>Daft Punk</h3>
         <span>-</span>
